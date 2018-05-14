29bfec660527fe0e053e5f4449666e3d084cb618
Søren Gregersen <s@grgrsn.dk>
2018-05-14 15:17:19 +0200

    Moved source code to Xcode Framework project

---
+++ CXDurationPicker/CXDurationPicker.h
@@ -0,0 +1,21 @@
+//
+//  CXDurationPicker.h
+//  CXDurationPicker
+//
+//  Created by Søren Gregersen on 14/05/2018.
+//  Copyright © 2018 Emplate ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CXDurationPicker.
+FOUNDATION_EXPORT double CXDurationPickerVersionNumber;
+
+//! Project version string for CXDurationPicker.
+FOUNDATION_EXPORT const unsigned char CXDurationPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXDurationPicker/PublicHeader.h>
+
+#import <CXDurationPicker/CXDurationPickerView.h>
+#import <CXDurationPicker/CXDurationPickerDate.h>
+#import <CXDurationPicker/CXDurationPickerMonthView.h>
